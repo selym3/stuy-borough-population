293828deabb4a1da179cd513e4d4dc1f640ce4f3
Myles Pasetsky <mp3blue99@gmail.com>
2021-05-28 13:09:21 -0400

    Add more config, updated images, and html files

--- main.py
+++ main.py
@@ -29,43 +29,67 @@ def format_list(L):
         return comma_separated + f', and {L[-1]}'
 
 def parse(filename):
-    
+    '''
+    this function parses the file in a way that's ideal for 
+    using matplotlib 
+
+    it returns (list of years), (list of boroughs), (map of borough -> list of populations)
+
+    a better/cooler representation of the data might be (map of year -> (map of borough -> population))
+    but this makes it harder to use matplot lib
+    '''
+
     def get_parts(l):
         return l.strip().split(',')
 
     with open(filename) as f:
+
         header = get_parts(f.readline())
         boroughs = [ validate_br(br) for br in header[1:] ]
+        
+        years = []
 
-        city_population = {}
+        city_population = {
+            borough: []
+            for borough
+            in boroughs
+        }
 
         for line in read_lines(f):
             data = [ int(part) for part in get_parts(line) ]
 
             year, populations = data[0], data[1:]
 
-            city_population[year] = {
-                borough: population
-                for borough, population
-                in zip(boroughs, populations)
-            }
+            years += [ year ]
 
-        return boroughs, city_population
+            for borough, population in zip(city_population, populations):
+                city_population[borough] += [ int(population) ]
+                
+
+        return years, boroughs, city_population
 
 import matplotlib.pyplot as plt
 
 class CityManager:
 
     def __init__(self, from_csv):
-        self.boroughs, self.populations = parse(from_csv)
-        self.years = list(self.populations.keys())
+        '''
+        class to encapsulate city population data
+
+        parameters-
+            from_csv - data file to read from
+
+        fields-
+            years - list of years (x-values)
+            boroughs - list of boroughs (in population map), help choose a y data set
+            populations - map of lists of populations (all possible y values)
+        '''
+
+        parsed = parse(from_csv)
+        self.years, self.boroughs, self.populations = parsed
 
     def get_borough_data(self, borough):
-        borough = validate_br(borough)
-        y = []
-        for populations in self.populations.values():
-            y += [ populations[borough] ]
-        return self.years, y
+        return self.years, self.populations[borough]
 
     def setup_borough(self, borough):
         borough = validate_br(borough)
@@ -74,10 +98,11 @@ class CityManager:
         plt.plot(
             years, 
             populations, 
-            label=f'{borough}'
+            label=formal_capitalize(borough)
         )
 
     def graph_boroughs(self, boroughs=None, savefile=None, show=True):
+        plt.close()
         boroughs = self.boroughs if boroughs is None else boroughs
 
         for borough in boroughs:
@@ -99,17 +124,22 @@ class CityManager:
 
 if __name__ == "__main__":
 
-    # Save the .GIF programatically
+    # Choose what the program should do
+    SAVE_IMAGE = True
+    SHOW_PLOT = False
+
+    # If going to save the images, select where
     SAVE_TYPE = '.png' #<-- GIF is not available
     
     MY_BOROUGH = 'images/my_borough' + SAVE_TYPE
     ALL_BOROUGHS = 'images/all_boroughs' + SAVE_TYPE
-    
-    SHOW_PLOT = True
+
+    if not SAVE_IMAGE:
+        MY_BOROUGH, ALL_BOROUGHS = None, None
 
     # Parse the CSV file
     CSV_PATH = "resources/nyc-population.csv"
-    
+
     manager = CityManager(from_csv=CSV_PATH)
 
     '''
